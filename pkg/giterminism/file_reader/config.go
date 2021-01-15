417961eede00bb55e3483b57fb30cf3570241d7b
Alexey Igrychev <alexey.igrychev@flant.com>
2021-01-15 20:23:55 +0000

    [giterminism manager] Refactoring and fixes
    - Update common error message
    - Update not found the werf config error message
    - Generalize code: readConfigurationFile, checkConfigurationFileExistence, isConfigurationFileExist
    - Change configurationFilesGlob method args
    - Fix error handling

--- pkg/giterminism/file_reader/config.go
+++ pkg/giterminism/file_reader/config.go
@@ -24,43 +24,59 @@ func (r FileReader) ReadConfig(ctx context.Context, customRelPath string) ([]byt
 		return r.readConfig(ctx, configPath)
 	}
 
-	return nil, r.prepareConfigNotFoundError(configRelPathList)
+	return nil, r.prepareConfigNotFoundError(ctx, configRelPathList)
 }
 
 func (r FileReader) isConfigExist(ctx context.Context, relPath string) (bool, error) {
-	if r.manager.LooseGiterminism() || r.manager.Config().IsUncommittedConfigAccepted() {
-		return r.isFileExist(relPath)
-	}
-
-	return r.isCommitFileExist(ctx, relPath)
+	return r.isConfigurationFileExist(ctx, relPath, func(_ string) (bool, error) {
+		return r.manager.Config().IsUncommittedConfigAccepted(), nil
+	})
 }
 
 func (r FileReader) readConfig(ctx context.Context, relPath string) ([]byte, error) {
-	if r.manager.LooseGiterminism() || r.manager.Config().IsUncommittedConfigAccepted() {
-		return r.readFile(relPath)
-	}
-
-	return r.readCommitConfig(ctx, relPath)
+	return r.readConfigurationFile(ctx, configErrorConfigType, relPath, func(relPath string) (bool, error) {
+		return r.manager.Config().IsUncommittedConfigAccepted(), nil
+	})
 }
 
 func (r FileReader) readCommitConfig(ctx context.Context, relPath string) ([]byte, error) {
 	return r.readCommitFile(ctx, relPath, func(ctx context.Context, relPath string) error {
-		return NewUncommittedFilesChangesError("werf config", relPath)
+		return NewUncommittedFilesChangesError(configErrorConfigType, relPath)
 	})
 }
 
-func (r FileReader) prepareConfigNotFoundError(configPathsToCheck []string) error {
-	var err error
-	if r.manager.LooseGiterminism() {
-		err = NewFilesNotFoundInTheProjectDirectoryError("werf config", configPathsToCheck...)
-	} else {
-		err = NewFilesNotFoundInTheProjectGitRepositoryError("werf config", configPathsToCheck...)
+func (r FileReader) prepareConfigNotFoundError(ctx context.Context, configPathsToCheck []string) error {
+	for _, configPath := range configPathsToCheck {
+		err := r.checkConfigurationFileExistence(ctx, configErrorConfigType, configPath, func(_ string) (bool, error) {
+			return r.manager.Config().IsUncommittedConfigAccepted(), nil
+		})
+
+		switch err.(type) {
+		case UncommittedFilesError, UncommittedFilesChangesError:
+			return err
+		}
 	}
 
-	return ConfigNotFoundError(err)
+	var configPath string
+	if len(configPathsToCheck) == 1 {
+		configPath = configPathsToCheck[0]
+	} else { // default werf config (werf.yaml, werf.yml)
+		configPath = "werf.yaml"
+	}
+
+	var err error
+	if r.manager.LooseGiterminism() {
+		err = NewFilesNotFoundInTheProjectDirectoryError(configErrorConfigType, configPath)
+	} else {
+		err = NewFilesNotFoundInTheProjectGitRepositoryError(configErrorConfigType, configPath)
+	}
+
+	return ConfigNotFoundError{err}
 }
 
-type ConfigNotFoundError error
+type ConfigNotFoundError struct {
+	error
+}
 
 func IsConfigNotFoundError(err error) bool {
 	switch err.(type) {
